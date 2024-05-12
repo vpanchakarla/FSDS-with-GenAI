7ee2ea6a42db51cca166784fdcb09af2f646f97d
vpanchakarla <praveen.panchakarla@gmail.com>
2024-05-12 09:31:58 +0300

    12-May-2024: Uploading all notes covered so far

---
+++ exceptionInPython/exception_python.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+#create a logger string
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_folder = "logs"
+log_file = "logs/running_log.log"
+
+os.makedirs(log_folder, exist_ok=True)
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_file)
+    ]
+)
+logger = logging.getLogger("mylog")
+
+def division(a,b):
+    try:
+        out = a/b
+        return out
+#        return round(out,2)
+    except Exception as e:
+        logger.info(e)
+
+out = division(4,0)
+print(out)
